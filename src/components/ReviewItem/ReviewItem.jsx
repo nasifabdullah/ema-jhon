10a2e71fdcdc4cb79565efc9eea104ac08f59ad4
nasifabdullah <nasifbinabdullah@gmail.com>
2023-04-07 01:33:43 +0600

    added review and proceed btn

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -15,7 +15,7 @@ const ReviewItem = ({ product, handleRemoveFromCart }) => {
           Price: <span className="orange-text">${price}</span>{" "}
         </p>
         <p>
-          Order Quantity: <span className="orange-text">${quantity}</span>{" "}
+          Order Quantity: <span className="orange-text">{quantity}</span>{" "}
         </p>
       </div>
       <button onClick={() => handleRemoveFromCart(id)} className="btn-delete"><FontAwesomeIcon className="delete-icon" icon={faTrashAlt} /></button>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb, getShoppingCart } from '../../utilities/fakedb';
+import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+
 const Shop = () => {
     const [products, setProducts] =useState([]);
     const [cart, setCart] = useState([])
@@ -54,6 +59,11 @@ const Shop = () => {
         addToDb(product.id)
       }
 
+      const handleClearCart =() =>{
+        setCart([]);
+        deleteShoppingCart();
+      }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -66,7 +76,14 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart 
+                cart={cart}
+                handleClearCart={handleClearCart}
+                >
+                <Link className='proceed-link' to={'/orders'}>
+                <button className='btn-proceed'>Review Order <FontAwesomeIcon icon={faArrowRight} /></button>
+                </Link>
+                </Cart>
             </div>
         </div>
     );
